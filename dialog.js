6e81131de81ce5f0af295bc815d54ec5232ee79d
hjjia <normal999@163.com>
2016-11-02 23:12:50 +0800

    Add files via upload

--- dialog.js
+++ dialog.js
@@ -13,22 +13,30 @@
 
 	// 创建Dialog对象  
 	var Dialog = {};
+	var $body  = $('body');
 
 	// 重置Dialog的原型链
 	Dialog.fn = Dialog.prototype = function () {};
 
 	// 定义公共函数
 	var openDialog = function(options) {
-		var type    = options.type    || 'open',
-			title   = options.title   || '',
-			content = options.content || '',
-			btn     = options.btn     || [''],
-			index   = options.index   || +new Date() + 99999,
-			yes     = options.yes     || function () {},
-			cancel  = options.cancel  || function () {},
-			seconds = options.seconds,
-			showBg  = options.showBg  || true,
-			html    = '';
+		var type      = options.type     || 'open',
+			title     = options.title    || '',
+			content   = options.content  || '',
+			btn       = options.btn      || [],
+			index     = options.index    || +new Date() + 99999,
+			yes       = options.yes      || function (index) {Dialog.close(index)},
+			cancel    = options.cancel   || function (index) {Dialog.close(index)},
+			seconds   = options.seconds,
+			showBg    = options.showBg   || true,
+			icon      = options.icon     || '',
+			html      = '',
+			innerHtml = '',
+			maxZIndex = 9999,
+			styleHtml = 'display:none;',
+			iconHtml  = '',
+			titleHtml = '',
+			mainHtml  = '';
 
 			// console.log(options,'options',title)
 
@@ -37,57 +45,87 @@
 			case 'msg':
 				break;
 			case 'confirm':
-				html = '';
-				html += '<div class="dialog dialog-' + index + '" style="">';
-				html += '<div class="dialog-inner">';
-				html += '<div class="dialog-close-btn">X</div>';
-				html += '<div class="dialog-title dialog-title-wrap">';
-				html += '<span>' + title + '</span>';
-				html += '</div>';
-				html += '<div class="dialog-content">' + content;
-				html += '</div>';
-				html += '<div class="dialog-footer">';
-				html += '<div class="btn-group">';
-				html += '<button class="dialog-btn dialog-ok-btn">确定</button>';
-				html += '<button class="dialog-btn dialog-cancel-btn">取消</button>';
-				html += '</div>';
-				html += '</div>';
-				html += '</div>';
-				html += '</div>';
+				innerHtml = '';
+				innerHtml += '<button class="dialog-btn dialog-ok-btn">' + btn[0] + '</button>';
+				innerHtml += '<button class="dialog-btn dialog-cancel-btn">' + btn[1] + '</button>';
 				break;
 			case 'open':
-				html = '';
-				html += '<div class="dialog">';
-				html += '<div class="dialog-inner">';
-				html += '<div class="dialog-close-btn">X</div>';
-				html += '<div class="dialog-title dialog-title-wrap">';
-				html += '<span>测试</span>';
-				html += '</div>';
-				html += '<div class="dialog-content">';
-				html += '</div>';
-				html += '<div class="dialog-footer">';
-				html += '<div class="btn-group">';
-				html += '<button class="dialog-btn dialog-ok-btn">确定</button>';
-				html += '<button class="dialog-btn dialog-cancel-btn">取消</button>';
-				html += '</div>';
-				html += '</div>';
-				html += '</div>';
-				html += '</div>';
-
+				innerHtml = '';
+				btn[0] &&　(innerHtml += '<button class="dialog-btn dialog-ok-btn">' + btn[0] + '</button>');
+				btn[1] &&  (innerHtml += '<button class="dialog-btn dialog-cancel-btn">' + btn[1] + '</button>');
 				break;
 		}
 
+		maxZIndex = getMaxZIndex(maxZIndex) + 1;
+		styleHtml += 'z-index:' + maxZIndex +';';
+		iconHtml  = icon ? icon : '';
+
+		title[1]   && (titleHtml = title[1]);
+		content[1] && (mainHtml  = content[1]);
+
+		html += '<div class="dialog dialog-' + index + '" style="' + styleHtml + '">';
+		html += '<div class="dialog-inner">';
+		html += '<div class="dialog-close-btn">X</div>';
+		html += '<div class="dialog-title dialog-title-wrap" ' + titleHtml + '>';
+		html += '<h1>' + title + '</h1>';
+		html += '</div>';
+		html += '<div class="dialog-content" style="' + mainHtml + '">'+ iconHtml + content;
+		html += '</div>';
+		html += '<div class="dialog-footer">';
+		html += '<div class="btn-group">';
+		html += innerHtml;
+		html += '</div>';
+		html += '</div>';
+		html += '</div>';
+		html += '</div>';
+
 		// 判断是否显示背景
-		showBg && openBg(index);
-		$('body').append(html);
+		showBg && openBg();
+		$body.append(html);
+		$('.dialog-' + index).fadeIn();
+
+		// 绑定事件
+		switch (type) {
+			case 'alert':
+				// 确定按钮回调函数
+				$body.on('click', '.dialog-ok-btn', function () {
+
+					// 先移除背景在移除弹框
+					closeBg();
+					yes && yes(index);
+				});
+				break;
+
+			case 'confirm':
+			case 'open':
+				// 确定按钮回调函数
+				$body.on('click', '.dialog-ok-btn', function () {
+					closeBg();
+					yes && yes(index);
+				});
+
+				// 取消按钮回调函数
+				$body.on('click', '.dialog-cancel-btn', function () {
+					closeBg();
+					cancel && cancel(index);
+				});
+				break;
+			case 'msg':
+				break;
+		}
+
+		// 关闭弹框
+		$body.on('click', '.dialog-close-btn', function () {
+			closeBg();
+			$('.dialog-' + index).remove();
+		});
 
 		// 位置居中
 		setPosition('.dialog-' + index);
 
 		// 监听浏览器窗口变化
 		$(window).resize(function () {
-			console.log('----');
-
+			// console.log('----');
 			setPosition('.dialog-' + index)
 		});
 		
@@ -145,7 +183,7 @@
 			// 判断当前弹框的宽度、高度是否大于当前屏幕的宽、高
 			if(width >= winWidth) {
 				width = winWidth - 100;
-				$Dialog.css('width',width + 'px');
+				$dialog.css('width',width + 'px');
 			}
 
 			if(height >= winHeight) {
@@ -155,7 +193,7 @@
 
 			docHeight > winHeight && (winWidth = winWidth - 17);
 
-			left      = (winWidth - width) / 2,
+			left      = (winWidth - width) / 2;
 			top       = (winHeight - height) / 2; 
 
 		// console.log(winWidth,winHeight,width,height,left,top,docHeight)
@@ -164,29 +202,81 @@
 			'left': left + 'px',
 			'top' : top + 'px'
 		});
-		console.log(winWidth,winHeight,'oooooooooo')
+		// console.log(winWidth,winHeight,'oooooooooo')
 
-	}
+	};
+
+	Dialog.close = function (index) {
+		console.log(index, $('.dialog-' + index),'iiii');
+		$('.dialog-' + index).remove();
+	};
+
+	Dialog.closeAll = function () {
+		$body.find('.dialog').remove();
+	};
 
 	// 显示背景
-	var openBg = function (index) {
-		var html = '<div class="dialog-bg dialog-bg-' + index + '">';
-		$('body').append(html);
-	}
+	var openBg = function () {
+		var html = '<div class="dialog-bg">';
+		$('.dialog-bg').length == 0 && ($body.append(html));
+
+		$body.on('click', '.dialog-bg', function () {
+			$(this).remove();
+			Dialog.closeAll();
+		})
+	};
+
+	var closeBg = function () {
+		$('.dialog').length <= 1 && ($('.dialog-bg').remove());
+	};
+
+	var getMaxZIndex = function (maxZIndex) {
+		var thisZIndex = 0;
+
+		$('.dialog, .dialog-msg').each(function () {
+			thisZIndex = $(this).css('z-index');
+
+			thisZIndex > maxZIndex && (maxZIndex = thisZIndex);
+		});
+
+		return maxZIndex;
+	};
 
 	// 确认弹框
-	Dialog.confirm = function (options) {
-		// var param = {},
-			// param.type    = 'confirm',
-			// param.title   = options.title,
-			// param.content = options.content || '',
-			// param.btn     = options.btn     || ['确定','取消'],
-			// param.yes     = options.yes,
-			// param.cancel  = options.cancel
+	Dialog.confirm = function (options, btn, yes, cancel) {
+		var param = {};
 
-			options.type = 'confirm';
+		param.type    = 'confirm';
 
-		return openDialog(options);
+		if(typeof options == 'object') {
+			param.title   = options.title;
+			param.content = options.content || '';
+			param.btn     = options.btn     || ['确定','取消'];
+			param.yes     = options.yes;
+			param.cancel  = options.cancel;
+		}
+		else {
+			param.content = options;
+			param.btn     = btn || ['确定','取消'];
+			param.yes     = yes;
+			param.cancel  = cancel;
+		}
+
+		return openDialog(param);
+	};
+
+	// 普通弹框
+	Dialog.open = function (options) {
+		var param = {};
+
+		param.type    = 'open';
+		param.title   = options.title;
+		param.content = options.content || '';
+		param.btn     = options.btn     || [];
+		param.yes     = options.yes;
+		param.cancel  = options.cancel;
+
+		return openDialog(param);
 	};
 
 	var moveDialogElem     = null,
@@ -209,4 +299,4 @@
 
 	win.Dialog = Dialog;
 
-})(jQuery,window)
+})(jQuery,window);
