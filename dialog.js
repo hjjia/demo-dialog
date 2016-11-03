1b7b509c3939476d6f5acc1c0ce46b625692d98d
hjjia <normal999@163.com>
2016-11-03 23:07:32 +0800

    Add files via upload
    add alert and msg

--- dialog.js
+++ dialog.js
@@ -21,14 +21,14 @@
 	// 定义公共函数
 	var openDialog = function(options) {
 		var type      = options.type     || 'open',
-			title     = options.title    || '',
+			title     = options.title    || '提示',
 			content   = options.content  || '',
 			btn       = options.btn      || [],
 			index     = options.index    || +new Date() + 99999,
 			yes       = options.yes      || function (index) {Dialog.close(index)},
 			cancel    = options.cancel   || function (index) {Dialog.close(index)},
-			seconds   = options.seconds,
-			showBg    = options.showBg   || true,
+			seconds   = options.seconds  || 10,
+			showBg    = options.showBg   || false,
 			icon      = options.icon     || '',
 			html      = '',
 			innerHtml = '',
@@ -38,11 +38,11 @@
 			titleHtml = '',
 			mainHtml  = '';
 
-			// console.log(options,'options',title)
-
 		// 判断是哪种类型的弹框
 		switch(type) {
 			case 'msg':
+				innerHtml = '';
+				innerHtml += '<div class="dialog-msg-content">' + content + '</div>';
 				break;
 			case 'confirm':
 				innerHtml = '';
@@ -54,6 +54,9 @@
 				btn[0] &&　(innerHtml += '<button class="dialog-btn dialog-ok-btn">' + btn[0] + '</button>');
 				btn[1] &&  (innerHtml += '<button class="dialog-btn dialog-cancel-btn">' + btn[1] + '</button>');
 				break;
+			case 'alert':
+				innerHtml = '';
+				btn[0] &&　(innerHtml += '<button class="dialog-btn dialog-ok-btn">' + btn[0] + '</button>');
 		}
 
 		maxZIndex = getMaxZIndex(maxZIndex) + 1;
@@ -63,21 +66,26 @@
 		title[1]   && (titleHtml = title[1]);
 		content[1] && (mainHtml  = content[1]);
 
-		html += '<div class="dialog dialog-' + index + '" style="' + styleHtml + '">';
-		html += '<div class="dialog-inner">';
-		html += '<div class="dialog-close-btn">X</div>';
-		html += '<div class="dialog-title dialog-title-wrap" ' + titleHtml + '>';
-		html += '<h1>' + title + '</h1>';
-		html += '</div>';
-		html += '<div class="dialog-content" style="' + mainHtml + '">'+ iconHtml + content;
-		html += '</div>';
-		html += '<div class="dialog-footer">';
-		html += '<div class="btn-group">';
-		html += innerHtml;
-		html += '</div>';
-		html += '</div>';
-		html += '</div>';
-		html += '</div>';
+		if(type == 'msg') {
+			html += '<div class="dialog dialog-msg dialog-' + index + ' dialog-msg-' + index + '" style="' + styleHtml + '">' + innerHtml + '</div>'
+		}
+		else {
+			html += '<div class="dialog dialog-' + index + '" style="' + styleHtml + '">';
+			html += '<div class="dialog-inner">';
+			html += '<div class="dialog-close-btn">X</div>';
+			html += '<div class="dialog-title dialog-title-wrap" ' + titleHtml + '>';
+			html += '<h1>' + title + '</h1>';
+			html += '</div>';
+			html += '<div class="dialog-content" style="' + mainHtml + '">'+ iconHtml + content;
+			html += '</div>';
+			html += '<div class="dialog-footer">';
+			html += '<div class="btn-group">';
+			html += innerHtml;
+			html += '</div>';
+			html += '</div>';
+			html += '</div>';
+			html += '</div>';
+		}
 
 		// 判断是否显示背景
 		showBg && openBg();
@@ -90,9 +98,9 @@
 				// 确定按钮回调函数
 				$body.on('click', '.dialog-ok-btn', function () {
 
+					yes && yes(index);
 					// 先移除背景在移除弹框
 					closeBg();
-					yes && yes(index);
 				});
 				break;
 
@@ -100,17 +108,21 @@
 			case 'open':
 				// 确定按钮回调函数
 				$body.on('click', '.dialog-ok-btn', function () {
-					closeBg();
 					yes && yes(index);
+					closeBg();
 				});
 
 				// 取消按钮回调函数
 				$body.on('click', '.dialog-cancel-btn', function () {
-					closeBg();
 					cancel && cancel(index);
+					closeBg();
 				});
 				break;
 			case 'msg':
+				seconds = seconds * 1000;
+				setTimeout(function() {
+					$('.dialog-msg-' + index).fadeOut().remove();
+				}, seconds);
 				break;
 		}
 
@@ -196,18 +208,13 @@
 			left      = (winWidth - width) / 2;
 			top       = (winHeight - height) / 2; 
 
-		// console.log(winWidth,winHeight,width,height,left,top,docHeight)
-
 		$dialog.css({
 			'left': left + 'px',
 			'top' : top + 'px'
 		});
-		// console.log(winWidth,winHeight,'oooooooooo')
-
 	};
 
 	Dialog.close = function (index) {
-		console.log(index, $('.dialog-' + index),'iiii');
 		$('.dialog-' + index).remove();
 	};
 
@@ -247,11 +254,13 @@
 		var param = {};
 
 		param.type    = 'confirm';
+		param.showBg  = true;
 
 		if(typeof options == 'object') {
 			param.title   = options.title;
 			param.content = options.content || '';
 			param.btn     = options.btn     || ['确定','取消'];
+			param.showBg  = options.showBg  || true;
 			param.yes     = options.yes;
 			param.cancel  = options.cancel;
 		}
@@ -273,8 +282,50 @@
 		param.title   = options.title;
 		param.content = options.content || '';
 		param.btn     = options.btn     || [];
+		param.showBg  = options.showBg  || true;
 		param.yes     = options.yes;
 		param.cancel  = options.cancel;
+
+		return openDialog(param);
+	};
+
+	// alert
+	Dialog.alert = function (options,btn, yes) {
+		var param = {};
+
+		param.type = 'alert';
+		param.showBg  = true;
+
+		if(typeof options == 'object') {
+			param.title   = options.title;
+			param.content = options.content || '';
+			param.btn     = options.btn     || ['确定'];
+			param.yes     = options.yes;
+			param.showBg  = options.showBg  || true;
+
+		}
+		else {
+			param.content = options;
+			param.btn     = btn     || ['确定'];
+			param.yes     = options.yes;
+		}
+
+		return openDialog(param);
+	};
+
+	Dialog.msg = function (options, seconds) {
+		var param = {};
+
+		param.type   = 'msg';
+
+		if(typeof options == 'object') {
+			param.content = options.content;
+			param.seconds = options.seconds;
+		}
+		else {
+			param.content = options;
+			param.seconds = seconds;
+		}
 
 		return openDialog(param);
 	};
